57a6e3afc8f60d6683de578df19870bb38754c3f
Dimon_Keval <kevaldima@gmail.com>
2021-03-19 11:49:29 +0100

    Initial commit

---
+++ src/main/java/com/example/demo/InstatryApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstatryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstatryApplication.class, args);
+	}
+
+}
