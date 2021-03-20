5a540b3a8c85e6c34e3500b8975968c28591f3cb
Dimon_Keval <kevaldima@gmail.com>
2021-03-20 13:29:05 +0100

    Complete controllers commit

--- src/main/java/com/example/demo/web/UserController.java
+++ src/main/java/com/example/demo/web/UserController.java
@@ -8,8 +8,11 @@ import com.example.demo.validations.ResponseErrorValidation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.ObjectUtils;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.security.Principal;
 
 @RestController
@@ -38,5 +41,16 @@ public class UserController {
         UserDTO userDTO = userFacade.userToUserDTO(user);
 
         return new ResponseEntity<>(userDTO, HttpStatus.OK);
+    }
+
+    @PostMapping("/update")
+    public ResponseEntity<Object> updateUser(@Valid @RequestBody UserDTO userDTO, BindingResult bindingResult, Principal principal) {
+        ResponseEntity<Object> errors = responseErrorValidation.mapValidationService(bindingResult);
+        if (!ObjectUtils.isEmpty(errors)) return errors;
+
+        User user = userService.updateUser(userDTO, principal);
+
+        UserDTO userUpdated = userFacade.userToUserDTO(user);
+        return new ResponseEntity<>(userUpdated, HttpStatus.OK);
     }
 }
